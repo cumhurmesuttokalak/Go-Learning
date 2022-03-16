1a6cd43c0262a578ecffa8439859c73b9129b128
cumhurmessuttokalak <79723267+cumhurmessuttokalak@users.noreply.github.com>
2022-03-16 19:41:47 +0300

    Deneme

--- Variables/consts.go
+++ Variables/consts.go
@@ -3,9 +3,10 @@ package main
 const sabit_1 = "deger 1"
 
 const (
-	sabit_2 = "deger 2"
-	sabit_3 = "deger 3"
-	sabit_4 = "deger 4"
+	sabit_2  = "deger 2"
+	sabit_3  = "deger 3"
+	sabit_4  = "deger 4"
+	sabit_12 = "deger 12"
 )
 
 const (
@@ -17,5 +18,5 @@ const (
 func main() {
 	//sabit_1 = "deger 1.1"
 
-	println(sabit_1, sabit_2, sabit_3, sabit_4, sabit_5, sabit_6, sabit_7)
+	println(sabit_1, sabit_2, sabit_3, sabit_4, sabit_5, sabit_6, sabit_7, sabit_12)
 }
